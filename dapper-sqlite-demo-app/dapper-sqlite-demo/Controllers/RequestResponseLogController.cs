a6f5f8f858ebd55a50c970c405b5d9580c11b8b4
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-04-06 14:28:57 +0200

    setup controller

---
+++ dapper-sqlite-demo-app/dapper-sqlite-demo/Controllers/RequestResponseLogController.cs
@@ -0,0 +1,38 @@
+﻿using dapper_sqlite_demo.RequestResponseLogMaster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_sqlite_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestResponseLogController : ControllerBase
+    {
+        private readonly IRequestResponseLogProvider _logProvider;
+        private readonly IRequestResponseLogRepo _logRepo;
+
+        public RequestResponseLogController(IRequestResponseLogProvider logProvider, IRequestResponseLogRepo logRepo)
+        {
+            _logProvider = logProvider;
+            _logRepo = logRepo;
+        }
+
+        // GET api/requestResponseLog
+        [HttpGet]
+        public async Task<IEnumerable<RequestResponseLog>> Get()
+        {
+            return await _logProvider.Get();
+        }
+
+        // POST api/requestResponseLog
+        [HttpPost]
+        public async Task Post([FromBody] RequestResponseLog log)
+        {
+            await _logRepo.Create(log);
+        }
+    }
+}
